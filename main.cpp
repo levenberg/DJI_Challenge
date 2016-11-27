af24e78dc2778474ea4ac408ea69a5f066a32dae
levenberg <353817175@qq.com>
2016-11-27 21:00:30 +0800

    add hello world script by cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    //! @note replace these two lines below to change to an other hard-driver level.
+	cout << "hello world, this is the first project in github!" << endl;
+
+    return 0;
+}
